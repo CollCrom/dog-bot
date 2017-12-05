426cb958d08b15892e5bf3752cfe91fb6ea306cd
Collin Cromwell <collcrom@gmail.com>
2017-12-05 13:05:08 -0600

    made the web scraper better

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 worker: node bot.js
-spawn: python web_scraper.py
--- web_scraper.py
+++ web_scraper.py
@@ -5,17 +5,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.PhantomJS()
-search_query = sys.argv[1].replace(' ', '%20')
+#search_query = sys.argv[1].replace(' ', '+')
 
-browser.get("https://www.pexels.com/search/" + search_query)
+browser.get("https://imgur.com/search/relevance/all?q_type=png&q_all=" + 'dog')
 
 pic_arr = [];
 
-photos = browser.find_elements_by_class_name("photo-item")
+photos = browser.find_elements_by_class_name("post")
 
 for photo in photos:
-	img_elem = photo.find_element_by_tag_name("img")
-	picture = img_elem.get_attribute("src")
+	img_elem = photo.find_element_by_tag_name("a")
+	picture = img_elem.get_attribute("href")
+	picture = picture.replace("/gallery", "") + '.png'
 	pic_arr.append(picture)
 
 if not pic_arr:
